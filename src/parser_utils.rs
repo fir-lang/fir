31f4ec34eb7c38fa135104d5d454bf4de24a32d2
Ömer Sinan Ağacan <omeragacan@gmail.com>
2025-01-06 09:18:22 +0100

    Add a debug mode assertion in parser utils

--- src/parser_utils.rs
+++ src/parser_utils.rs
@@ -1,6 +1,9 @@
 pub(crate) fn parse_char_lit(text: &str) -> char {
     let mut chars = text.chars();
-    chars.next(); // skip '
+
+    let quote = chars.next(); // skip '
+    debug_assert_eq!(quote, Some('\''));
+
     let char = chars.next().unwrap();
     if char == '\\' {
         match chars.next().unwrap() {
