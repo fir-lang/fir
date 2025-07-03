ffa8e15722e188b682cc9b31b0608d91290ebc18
Ömer Sinan Ağacan <omeragacan@gmail.com>
2025-07-03 21:29:42 +0100

    Compiler: skip lexer tests with wide chars for now

--- compiler/tests/tokenize.sh
+++ compiler/tests/tokenize.sh
@@ -4,8 +4,30 @@ cargo build --release
 
 exit_code=0
 
+# These files have wide (as in "displayed width" defined my unicode)
+# characters, and we don't have a Fir library yet to get character widths and
+# update columns properly with wide chars.
+skip_files=(
+    "tests/Char.fir"
+    "tests/StrBuf.fir"
+)
+
 for f in **/*.fir; do
+    skip=false
+    for skip_f in "${skip_files[@]}"; do
+        if [[ "$f" == "$skip_f" ]]; then
+            echo "$f: skipping"
+            skip=true
+            break
+        fi
+    done
+
+    if $skip; then
+        continue
+    fi
+
     echo $f
+
     compiler_output=$(./target/release/fir compiler/Lexer.fir --main lexerDumpTokens -- "$f")
     interpreter_output=$(./target/release/fir --tokenize "$f")
     diff -u <(echo "$compiler_output") <(echo "$interpreter_output")
