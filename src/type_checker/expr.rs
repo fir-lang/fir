40963d6d7b176df1e70cf3dd3bd5d79f9a61447b
Ömer Sinan Ağacan <omeragacan@gmail.com>
2025-11-22 22:13:57 +0000

    Starting expr checking

--- src/type_checker/expr.rs
+++ src/type_checker/expr.rs
@@ -39,10 +39,11 @@ pub(super) fn check_expr(
             user_ty_args,
             ty_args,
         }) => {
-            debug_assert!(ty_args.is_empty());
+            assert!(ty_args.is_empty());
 
             // Check if local.
             if let Some(ty) = tc_state.env.get(var) {
+                assert!(user_ty_args.is_empty());
                 return (
                     unify_expected_ty(
                         ty.clone(),
