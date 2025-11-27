d230deb4ccbc3d1b629f3c9064f97fd043ce93d7
Ömer Sinan Ağacan <omeragacan@gmail.com>
2025-11-27 13:43:14 +0000

    Compiler: minor refactoring in the lexer and scanner
    - Rename dump functions to clarify that they're dumping interpreter
      tokens and compiler has different tokens.
    
    - Rewrite documentation of dump functions and rename the `combineTokens`
      function to make it clear that they're for comparing generated tokens
      with the interpreter's generated tokens.

--- Compiler/tests/scan.sh
+++ Compiler/tests/scan.sh
@@ -22,7 +22,7 @@ source "${SCRIPT_DIR}/common.sh"
 
 for f in "${files[@]}"; do
     echo $f
-    compiler_output=$(./target/release/fir Compiler/Scanner.fir --main scannerDumpTokens -- "$f")
+    compiler_output=$(./target/release/fir Compiler/Scanner.fir --main scannerDumpInterpreterTokens -- "$f")
     interpreter_output=$(./target/release/fir --scan "$f")
     diff -u <(echo "$interpreter_output") <(echo "$compiler_output")
     if [ $? -ne 0 ]; then
