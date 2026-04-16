63b1679298b6bd03517d052da97a3bdd67b12707
Ömer Sinan Ağacan <omeragacan@gmail.com>
2026-04-16 15:38:45 +0100

    Modules: raise an error when a name originates from different modules
    (#195)

--- src/type_checker/module_env.rs
+++ src/type_checker/module_env.rs
@@ -10,13 +10,113 @@ use crate::utils::loc_display;
 ///
 /// Names can be either unprefixed (from direct/selective imports) or prefixed (from
 /// `import [Foo as P]`, accessed as `P/name`).
-#[derive(Debug, Clone, Default)]
+#[derive(Debug, Default)]
 pub(crate) struct ModuleEnv {
     /// Unprefixed names.
-    names: HashMap<Name, Id>,
+    names: NameMap,
 
     /// Prefixed names: prefix -> name -> id.
-    prefixed: HashMap<Name, HashMap<Name, Id>>,
+    prefixed: HashMap<Name, NameMap>,
+}
+
+/// Maps names to ids. Allows overriding imported names with defined names.
+///
+/// When a name is imported multiple times, collects all imported ids, to allow error reporting on
+/// use.
+#[derive(Debug, Default)]
+struct NameMap {
+    map: HashMap<Name, IdSet>,
+}
+
+#[derive(Debug)]
+struct IdSet {
+    // `HashSet<Id>` separate from a `defined` field to be able to provide an `iter` method that
+    // yields all `Id`s.
+    //
+    // When `defined` is set, there must be only one element in the set.
+    ids: HashSet<Id>,
+    defined: bool,
+}
+
+impl NameMap {
+    fn add_defined(&mut self, name: &Name, id: Id, loc: &ast::Loc) {
+        match self.map.entry(name.clone()) {
+            Entry::Occupied(mut entry) => {
+                if entry.get().defined {
+                    panic!("{}: {} defined multiple times", loc_display(loc), name);
+                }
+                entry.get_mut().ids.clear();
+                entry.get_mut().ids.insert(id);
+                entry.get_mut().defined = true;
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(IdSet {
+                    ids: std::iter::once(id).collect(),
+                    defined: true,
+                });
+            }
+        }
+    }
+
+    /// Returns whether a new import is added.
+    fn add_imported(&mut self, name: Name, id: Id) -> bool {
+        match self.map.entry(name.clone()) {
+            Entry::Occupied(mut entry) => {
+                if entry.get().defined {
+                    // Imported names don't shadow defined ones.
+                    return false;
+                }
+                entry.get_mut().ids.insert(id)
+            }
+            Entry::Vacant(entry) => {
+                entry.insert(IdSet {
+                    ids: std::iter::once(id).collect(),
+                    defined: false,
+                });
+                true
+            }
+        }
+    }
+
+    fn get(&self, name: &Name, loc: &ast::Loc) -> Id {
+        match self.map.get(name) {
+            None => {
+                panic!("{}: Unbound name {}", loc_display(loc), name)
+            }
+            Some(id_set) => {
+                debug_assert!(!id_set.ids.is_empty());
+                if id_set.ids.len() == 1 {
+                    id_set.ids.iter().next().cloned().unwrap()
+                } else {
+                    let mut msg = String::new();
+                    msg.push_str(&format!(
+                        "{}: Name {name} imported from multiple modules:\n",
+                        loc_display(loc)
+                    ));
+                    for id in id_set.ids.iter() {
+                        msg.push_str(&format!("- {}", id.module()));
+                        msg.push('\n');
+                    }
+                    panic!("{msg}");
+                }
+            }
+        }
+    }
+
+    fn iter(&self, module_path: &ModulePath) -> impl Iterator<Item = (&Name, &Id)> {
+        self.map.iter().map(move |(name, id_set)| {
+            if id_set.ids.len() == 1 {
+                (name, id_set.ids.iter().next().unwrap())
+            } else {
+                let mut msg = format!("{module_path} exports multiple {name}s:\n");
+                for id in &id_set.ids {
+                    msg.push_str(&format!("{}/{}", id.module(), id.name()));
+                    msg.push('\n');
+                }
+                panic!("{msg}")
+            }
+        })
+    }
 }
 
 impl ModuleEnv {
@@ -27,11 +127,7 @@ impl ModuleEnv {
         loc: &ast::Loc,
     ) -> Id {
         match mod_prefix {
-            None => self
-                .names
-                .get(name)
-                .cloned()
-                .unwrap_or_else(|| panic!("{}: Unbound name {}", loc_display(loc), name)),
+            None => self.names.get(name, loc),
             Some(path) => {
                 let segments = path.segments();
                 assert_eq!(
@@ -42,8 +138,8 @@ impl ModuleEnv {
                 );
                 let prefix = Name::from(&segments[0]);
                 self.prefixed
-                    .get(&Name::from(&segments[0]))
-                    .and_then(|prefix_map| prefix_map.get(name).cloned())
+                    .get(&prefix)
+                    .map(|name_map| name_map.get(name, loc))
                     .unwrap_or_else(|| {
                         panic!("{}: Unbound name {}/{}", loc_display(loc), prefix, name)
                     })
@@ -77,24 +173,27 @@ pub(crate) fn generate_module_envs(pgm: &LoadedPgm) -> HashMap<ModulePath, Modul
                 for decl in module.decls.iter() {
                     match &decl.node {
                         ast::TopDecl::Type(ty_decl) => {
-                            env.names.insert(
-                                ty_decl.node.name.clone(),
+                            env.names.add_defined(
+                                &ty_decl.node.name,
                                 Id::new(module_path, &ty_decl.node.name),
+                                &decl.loc,
                             );
                         }
 
                         ast::TopDecl::Trait(trait_decl) => {
-                            env.names.insert(
-                                trait_decl.node.name.node.clone(),
+                            env.names.add_defined(
+                                &trait_decl.node.name.node,
                                 Id::new(module_path, &trait_decl.node.name.node),
+                                &decl.loc,
                             );
                         }
 
                         ast::TopDecl::Fun(fun_decl) => {
                             if fun_decl.node.parent_ty.is_none() {
-                                env.names.insert(
-                                    fun_decl.node.name.node.clone(),
+                                env.names.add_defined(
+                                    &fun_decl.node.name.node,
                                     Id::new(module_path, &fun_decl.node.name.node),
+                                    &decl.loc,
                                 );
                             }
                         }
@@ -121,6 +220,7 @@ pub(crate) fn generate_module_envs(pgm: &LoadedPgm) -> HashMap<ModulePath, Modul
                     let imported_module_env = envs.remove(&module_import.path).unwrap();
                     let mut importing_module_env = envs.remove(module_path).unwrap();
                     updated |= import(
+                        &module_import.path,
                         &mut importing_module_env,
                         &imported_module_env,
                         &module_import.kind,
@@ -136,11 +236,16 @@ pub(crate) fn generate_module_envs(pgm: &LoadedPgm) -> HashMap<ModulePath, Modul
 }
 
 /// Returns whether a new name was imported.
-fn import(importing_env: &mut ModuleEnv, imported_env: &ModuleEnv, kind: &ImportKind) -> bool {
+fn import(
+    imported_module_path: &ModulePath,
+    importing_env: &mut ModuleEnv,
+    imported_env: &ModuleEnv,
+    kind: &ImportKind,
+) -> bool {
     let mut updated = false;
     match kind {
         ImportKind::Direct { filter } => {
-            for (imported_name, imported_id) in imported_env.names.iter() {
+            for (imported_name, imported_id) in imported_env.names.iter(imported_module_path) {
                 // Underscored names are not imported for direct use, but they can be used with a
                 // prefix.
                 if imported_name.starts_with('_') {
@@ -153,22 +258,16 @@ fn import(importing_env: &mut ModuleEnv, imported_env: &ModuleEnv, kind: &Import
                     },
                     None => imported_name.clone(),
                 };
-                if let std::collections::hash_map::Entry::Vacant(e) =
-                    importing_env.names.entry(local_name)
-                {
-                    e.insert(imported_id.clone());
-                    updated = true;
-                }
+                updated |= importing_env
+                    .names
+                    .add_imported(local_name, imported_id.clone());
             }
         }
 
         ImportKind::Prefixed { prefix } => {
             let prefix_env = importing_env.prefixed.entry(prefix.clone()).or_default();
-            for (imported_name, imported_id) in imported_env.names.iter() {
-                if !prefix_env.contains_key(imported_name) {
-                    prefix_env.insert(imported_name.clone(), imported_id.clone());
-                    updated = true;
-                }
+            for (imported_name, imported_id) in imported_env.names.iter(imported_module_path) {
+                updated |= prefix_env.add_imported(imported_name.clone(), imported_id.clone());
             }
         }
     }
